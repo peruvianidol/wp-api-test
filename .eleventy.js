b06ff4d0dd55a44d91fb2413a117ba2cd2ae4213
Mike Aparicio <peruvianidol@gmail.com>
2023-09-06 12:40:22 -0500

    Update url to tqu and add author to post page

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
+const { DateTime } = require('luxon');
+
 module.exports = function(eleventyConfig) {
   require("dotenv").config();
+
+  eleventyConfig.addFilter('longDate', dateObj => {
+    return DateTime.fromISO(dateObj, {zone: 'utc'}).toFormat('LLLL d, yyyy');
+  });
+
+  eleventyConfig.addFilter('w3Date', dateObj => {
+    return DateTime.fromISO(dateObj, {zone: 'utc'}).toISO();
+  });
+  
   return {
     dir: {
       input: '_src',
---
+++ _src/Untitled-1.js
@@ -0,0 +1,259 @@
+{
+  found: 3,
+  posts: [
+    {
+      ID: 10,
+      site_ID: 222015909,
+      author: [Object],
+      date: '2023-08-04T11:11:10-05:00',
+      modified: '2023-08-04T11:11:10-05:00',
+      title: 'Testing WordPress as a headless CMS with Eleventy',
+      URL: 'https://peruvianidol.wordpress.com/2023/08/04/testing-wordpress-as-a-headless-cms-with-eleventy/',
+      short_URL: 'https://wp.me/pf1ytL-a',
+      content: '\n' +
+        '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium molestias accusantium illum tenetur culpa fugit reprehenderit id vel in aut, nesciunt tempora consectetur quos cupiditate officia! Dolores sequi harum non.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Quae dolores placeat laudantium, est dolorem quasi. Aspernatur, vitae quidem placeat quas suscipit aliquid, vel saepe, veritatis esse tempora porro dolore fuga a nostrum fugiat deleniti voluptas? Aperiam, voluptas nemo.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Laborum, quas temporibus ea reprehenderit nobis tenetur neque nihil, ratione adipisci omnis repudiandae voluptatibus corporis autem placeat esse dolore tempora praesentium porro non obcaecati voluptatem reiciendis maxime id? Minus, quis.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Repellendus deleniti fugiat quaerat, id voluptatibus quo, cumque fugit amet minus dolore tempora, iusto illum sequi. Quos deserunt doloribus odit dolorum iusto, consectetur nam nesciunt consequatur est fugiat earum? Numquam!</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Dolor nisi, sapiente aliquid sed doloremque eos sit repellat fugit, aperiam aut reiciendis esse! Ut minima odio numquam, recusandae, laborum nihil totam voluptatibus hic, quidem maiores doloremque vero necessitatibus ducimus.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Eligendi voluptatum dolorem tempore, fuga expedita similique ea labore vero facere veritatis quaerat dolorum, aliquam animi vitae voluptate, obcaecati officia aut necessitatibus. Unde dolor corrupti reprehenderit ducimus, veritatis repudiandae optio!</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Ab unde deleniti mollitia animi consectetur facilis magni fuga, qui quisquam asperiores sit dolore suscipit doloribus minima temporibus corporis. Sapiente cupiditate ipsa nemo nihil inventore dicta repudiandae delectus molestias reiciendis!</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Magni esse, aperiam ratione aspernatur quod quo veniam deleniti doloribus aliquam! Ducimus, similique. Nemo numquam voluptate recusandae laboriosam amet sint necessitatibus, corrupti nihil tempore eos facere quo rem rerum totam!</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Tenetur neque eligendi sunt porro repudiandae ipsam eos, cupiditate vero temporibus cum mollitia molestiae totam voluptate ex maxime placeat consectetur, exercitationem ullam corporis possimus ad odio nostrum aperiam voluptatem? Harum.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Ducimus reiciendis rerum, accusamus animi nemo recusandae quibusdam sit. Dicta deserunt amet dolorum repellendus alias quam quos iste sequi, repudiandae aspernatur, ducimus quae doloremque nostrum tenetur earum ea ut accusantium!</p>\n',
+      excerpt: '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium molestias accusantium illum tenetur culpa fugit reprehenderit id vel in aut, nesciunt tempora consectetur quos cupiditate officia! Dolores sequi harum non. Quae dolores placeat laudantium, est dolorem quasi. Aspernatur, vitae quidem placeat quas suscipit aliquid, vel saepe, veritatis esse tempora porro dolore fuga a nostrum fugiat [&hellip;]</p>\n',
+      slug: 'testing-wordpress-as-a-headless-cms-with-eleventy',
+      guid: 'https://peruvianidol.wordpress.com/?p=10',
+      status: 'publish',
+      sticky: false,
+      password: '',
+      parent: false,
+      type: 'post',
+      discussion: [Object],
+      likes_enabled: true,
+      sharing_enabled: true,
+      like_count: 0,
+      i_like: false,
+      is_reblogged: false,
+      is_following: false,
+      global_ID: '6b37fff78b882479d110573bc4007ab5',
+      featured_image: '',
+      post_thumbnail: null,
+      format: 'standard',
+      geo: false,
+      menu_order: 0,
+      page_template: '',
+      publicize_URLs: [],
+      terms: [Object],
+      tags: {},
+      categories: [Object],
+      attachments: {},
+      attachment_count: 0,
+      metadata: [],
+      meta: [Object],
+      capabilities: [Object],
+      other_URLs: {}
+    },
+    {
+      ID: 5,
+      site_ID: 222015909,
+      author: [Object],
+      date: '2023-08-04T11:03:35-05:00',
+      modified: '2023-08-04T11:03:35-05:00',
+      title: 'Chapter One',
+      URL: 'https://peruvianidol.wordpress.com/2023/08/04/chapter-one/',
+      short_URL: 'https://wp.me/pf1ytL-5',
+      content: '\n' +
+        '<p>The studio was filled with the rich odour of roses, and when the light summer wind stirred amidst the trees of the garden, there came through the open door the heavy scent of the lilac, or the more delicate perfume of the pink-flowering thorn.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>From the corner of the divan of Persian saddle-bags on which he was lying, smoking, as was his custom, innumerable cigarettes, Lord Henry Wotton could just catch the gleam of the honey-sweet and honey-coloured blossoms of a laburnum, whose tremulous branches seemed hardly able to bear the burden of a beauty so flamelike as theirs; and now and then the fantastic shadows of birds in flight flitted across the long tussore-silk curtains that were stretched in front of the huge window, producing a kind of momentary Japanese effect, and making him think of those pallid, jade-faced painters of Tokyo who, through the medium of an art that is necessarily immobile, seek to convey the sense of swiftness and motion. The sullen murmur of the bees shouldering their way through the long unmown grass, or circling with monotonous insistence round the dusty gilt horns of the straggling woodbine, seemed to make the stillness more oppressive. The dim roar of London was like the bourdon note of a distant organ.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>In the centre of the room, clamped to an upright easel, stood the full-length portrait of a young man of extraordinary personal beauty, and in front of it, some little distance away, was sitting the artist himself, Basil Hallward, whose sudden disappearance some years ago caused, at the time, such public excitement and gave rise to so many strange conjectures.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>As the painter looked at the gracious and comely form he had so skilfully mirrored in his art, a smile of pleasure passed across his face, and seemed about to linger there. But he suddenly started up, and closing his eyes, placed his fingers upon the lids, as though he sought to imprison within his brain some curious dream from which he feared he might awake.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“It is your best work, Basil, the best thing you have ever done,” said Lord Henry languidly. “You must certainly send it next year to the Grosvenor. The Academy is too large and too vulgar. Whenever I have gone there, there have been either so many people that I have not been able to see the pictures, which was dreadful, or so many pictures that I have not been able to see the people, which was worse. The Grosvenor is really the only place.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“I don’t think I shall send it anywhere,” he answered, tossing his head back in that odd way that used to make his friends laugh at him at Oxford. “No, I won’t send it anywhere.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Lord Henry elevated his eyebrows and looked at him in amazement through the thin blue wreaths of smoke that curled up in such fanciful whorls from his heavy, opium-tainted cigarette. “Not send it anywhere? My dear fellow, why? Have you any reason? What odd chaps you painters are! You do anything in the world to gain a reputation. As soon as you have one, you seem to want to throw it away. It is silly of you, for there is only one thing in the world worse than being talked about, and that is not being talked about. A portrait like this would set you far above all the young men in England, and make the old men quite jealous, if old men are ever capable of any emotion.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“I know you will laugh at me,” he replied, “but I really can’t exhibit it. I have put too much of myself into it.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>Lord Henry stretched himself out on the divan and laughed.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“Yes, I knew you would; but it is quite true, all the same.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“Too much of yourself in it! Upon my word, Basil, I didn’t know you were so vain; and I really can’t see any resemblance between you, with your rugged strong face and your coal-black hair, and this young Adonis, who looks as if he was made out of ivory and rose-leaves. Why, my dear Basil, he is a Narcissus, and you—well, of course you have an intellectual expression and all that. But beauty, real beauty, ends where an intellectual expression begins. Intellect is in itself a mode of exaggeration, and destroys the harmony of any face. The moment one sits down to think, one becomes all nose, or all forehead, or something horrid. Look at the successful men in any of the learned professions. How perfectly hideous they are! Except, of course, in the Church. But then in the Church they don’t think. A bishop keeps on saying at the age of eighty what he was told to say when he was a boy of eighteen, and as a natural consequence he always looks absolutely delightful. Your mysterious young friend, whose name you have never told me, but whose picture really fascinates me, never thinks. I feel quite sure of that. He is some brainless beautiful creature who should be always here in winter when we have no flowers to look at, and always here in summer when we want something to chill our intelligence. Don’t flatter yourself, Basil: you are not in the least like him.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“You don’t understand me, Harry,” answered the artist. “Of course I am not like him. I know that perfectly well. Indeed, I should be sorry to look like him. You shrug your shoulders? I am telling you the truth. There is a fatality about all physical and intellectual distinction, the sort of fatality that seems to dog through history the faltering steps of kings. It is better not to be different from one’s fellows. The ugly and the stupid have the best of it in this world. They can sit at their ease and gape at the play. If they know nothing of victory, they are at least spared the knowledge of defeat. They live as we all should live—undisturbed, indifferent, and without disquiet. They neither bring ruin upon others, nor ever receive it from alien hands. Your rank and wealth, Harry; my brains, such as they are—my art, whatever it may be worth; Dorian Gray’s good looks—we shall all suffer for what the gods have given us, suffer terribly.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“Dorian Gray? Is that his name?” asked Lord Henry, walking across the studio towards Basil Hallward.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“Yes, that is his name. I didn’t intend to tell it to you.”</p>\n',
+      excerpt: '<p>The studio was filled with the rich odour of roses, and when the light summer wind stirred amidst the trees of the garden, there came through the open door the heavy scent of the lilac, or the more delicate perfume of the pink-flowering thorn. From the corner of the divan of Persian saddle-bags on which [&hellip;]</p>\n',
+      slug: 'chapter-one',
+      guid: 'https://peruvianidol.wordpress.com/2023/08/04/chapter-one/',
+      status: 'publish',
+      sticky: false,
+      password: '',
+      parent: false,
+      type: 'post',
+      discussion: [Object],
+      likes_enabled: true,
+      sharing_enabled: false,
+      like_count: 0,
+      i_like: false,
+      is_reblogged: false,
+      is_following: false,
+      global_ID: '5f95a4fa3b7e4af1ca64ff56feebc4ff',
+      featured_image: '',
+      post_thumbnail: null,
+      format: 'standard',
+      geo: false,
+      menu_order: 0,
+      page_template: '',
+      publicize_URLs: [],
+      terms: [Object],
+      tags: {},
+      categories: [Object],
+      attachments: {},
+      attachment_count: 0,
+      metadata: [Array],
+      meta: [Object],
+      capabilities: [Object],
+      other_URLs: {}
+    },
+    {
+      ID: 6,
+      site_ID: 222015909,
+      author: [Object],
+      date: '2023-08-04T11:03:34-05:00',
+      modified: '2023-08-04T11:03:34-05:00',
+      title: 'Chapter Two',
+      URL: 'https://peruvianidol.wordpress.com/2023/08/04/chapter-two/',
+      short_URL: 'https://wp.me/pf1ytL-6',
+      content: '\n' +
+        '<p>“Oh, I can’t explain. When I like people immensely, I never tell their names to any one. It is like surrendering a part of them. I have grown to love secrecy. It seems to be the one thing that can make modern life mysterious or marvellous to us. The commonest thing is delightful if one only hides it. When I leave town now I never tell my people where I am going. If I did, I would lose all my pleasure. It is a silly habit, I dare say, but somehow it seems to bring a great deal of romance into one’s life. I suppose you think me awfully foolish about it?”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“Not at all,” answered Lord Henry, “not at all, my dear Basil. You seem to forget that I am married, and the one charm of marriage is that it makes a life of deception absolutely necessary for both parties. I never know where my wife is, and my wife never knows what I am doing. When we meet—we do meet occasionally, when we dine out together, or go down to the Duke’s—we tell each other the most absurd stories with the most serious faces. My wife is very good at it—much better, in fact, than I am. She never gets confused over her dates, and I always do. But when she does find me out, she makes no row at all. I sometimes wish she would; but she merely laughs at me.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“I hate the way you talk about your married life, Harry,” said Basil Hallward, strolling towards the door that led into the garden. “I believe that you are really a very good husband, but that you are thoroughly ashamed of your own virtues. You are an extraordinary fellow. You never say a moral thing, and you never do a wrong thing. Your cynicism is simply a pose.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“Being natural is simply a pose, and the most irritating pose I know,” cried Lord Henry, laughing; and the two young men went out into the garden together and ensconced themselves on a long bamboo seat that stood in the shade of a tall laurel bush. The sunlight slipped over the polished leaves. In the grass, white daisies were tremulous.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>After a pause, Lord Henry pulled out his watch. “I am afraid I must be going, Basil,” he murmured, “and before I go, I insist on your answering a question I put to you some time ago.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“What is that?” said the painter, keeping his eyes fixed on the ground.</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“You know quite well.”</p>\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<p>“I do not, Harry.”</p>\n',
+      excerpt: '<p>“Oh, I can’t explain. When I like people immensely, I never tell their names to any one. It is like surrendering a part of them. I have grown to love secrecy. It seems to be the one thing that can make modern life mysterious or marvellous to us. The commonest thing is delightful if one [&hellip;]</p>\n',
+      slug: 'chapter-two',
+      guid: 'https://peruvianidol.wordpress.com/2023/08/04/chapter-two/',
+      status: 'publish',
+      sticky: false,
+      password: '',
+      parent: false,
+      type: 'post',
+      discussion: [Object],
+      likes_enabled: true,
+      sharing_enabled: false,
+      like_count: 0,
+      i_like: false,
+      is_reblogged: false,
+      is_following: false,
+      global_ID: '8f8a236e9e1fb87c0d878acb7bb85678',
+      featured_image: '',
+      post_thumbnail: null,
+      format: 'standard',
+      geo: false,
+      menu_order: 0,
+      page_template: '',
+      publicize_URLs: [],
+      terms: [Object],
+      tags: {},
+      categories: [Object],
+      attachments: {},
+      attachment_count: 0,
+      metadata: [Array],
+      meta: [Object],
+      capabilities: [Object],
+      other_URLs: {}
+    }
+  ],
+  meta: {
+    links: {
+      counts: 'https://public-api.wordpress.com/rest/v1.1/sites/222015909/post-counts/post'
+    },
+    wpcom: true
+  }
+}
